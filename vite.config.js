4565e1f9b013873347b2423b1e10ec83bfe9b41a
Lorand78 <mlorand78@gmail.com>
2025-05-20 11:22:33 +0200

    version 2.0 deployed

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vitePluginRequire from 'vite-plugin-require'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mybooks/',
   plugins: [
     vue(),
     // vitePluginRequire()
